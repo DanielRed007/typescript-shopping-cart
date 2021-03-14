33d53e6075a4d5e9588556c46f2948b4a19df9c9
DanielRed007 <danielrojo1927@gmail.com>
2021-03-14 12:30:10 -0300

    updating tests

--- src/test/shopping.cart.test.ts
+++ src/test/shopping.cart.test.ts
@@ -14,7 +14,7 @@ describe("Shopping Cart", () => {
 
   test("Add item to shopping cart", async () => {
     const quantity = 7;
-    const id = 29;
+    const id = 49;
 
     const product = await Product.findOne({ product_id: id });
     shoppingCart.addToCart(product, quantity);
@@ -23,7 +23,42 @@ describe("Shopping Cart", () => {
 
     expect(cart.items.length).toBeGreaterThan(0);
     const total = cart.formattedTotals;
-    expect(total).toBe("$489.93");
+    expect(total).toBe("$349.93");
+  });
+
+  test("Update item from shopping cart", async () => {
+    const ids = [14, 17];
+    const quantities = [3, 3];
+
+    const cartData = {
+      items: [
+        {
+          id: 14,
+          title:
+            "microsoft sql server standard edition 2005 64 bit cd/dvd 5 client",
+          price: 1849,
+          quantity: 1,
+          image: "9.jpeg",
+          formattedPrice: "$1,849.00",
+        },
+        {
+          id: 17,
+          title: "omnioutliner professional 3.0",
+          price: 69.95,
+          quantity: 1,
+          image: "6.jpeg",
+          formattedPrice: "$69.95",
+        },
+      ],
+      totals: 1918.95,
+      formattedTotals: "$1,918.95",
+    };
+
+    shoppingCart.data = cartData;
+
+    shoppingCart.updateCart(ids, quantities);
+
+    expect(shoppingCart.data.totals).toBe(5756.85);
   });
 
   test("Remove item from shopping cart", async () => {
@@ -31,13 +66,13 @@ describe("Shopping Cart", () => {
 
     const product = await Product.findOne({ product_id: id });
     expect(product._id).toBe("5795e57b7078f66363833442");
-
     shoppingCart.removeFromCart(product.product_id);
     shoppingCart.calculateTotals();
 
     expect(shoppingCart.data.length).toBeUndefined();
-    expect(shoppingCart.data.formattedTotals).toBe("$0.00");
   });
+
+  test("Empty shopping cart", () => {});
 
   afterAll(async () => {
     await mongoose.connection.close();
